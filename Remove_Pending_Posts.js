ad603aa70dcc5ad15b797a6ff4e40768a8ec53d6
Le Hau <45052513+j2teamlbh@users.noreply.github.com>
2019-05-17 08:01:43 +0700

    update

--- Remove_Pending_Posts.js
+++ Remove_Pending_Posts.js
@@ -1,7 +1,6 @@
 var fb_dtsg = require('DTSGInitialData').token;
 var gid = require('ScriptPath').getPageInfo().extraData.entity_id;
 var uid = require('CurrentUserInitialData').USER_ID;
-
 var get_pending_posts = (gid) => {
     return fetch('https://www.facebook.com/api/graphql/', {
             body: `fb_dtsg=${encodeURIComponent(fb_dtsg)}&__a=1&q=${encodeURIComponent(`node(${gid}){group_pending_stories.first(1000){edges{node{id,actors{id,name},message{text},id,creation_time}}}}`)}`,
@@ -13,7 +12,6 @@ var get_pending_posts = (gid) => {
         .then(res => res.json())
         .then(res => res[gid].group_pending_stories.edges)
 }
-
 var decline_post = (post_id, actor_id, gid, cb) => {
     let post_data = {
         q: 'Mutation GroupPendingPostAction : GroupDeletePendingStoryResponsePayload {group_delete_pending_story(<input>) {story {id},client_mutation_id}}',
@@ -26,11 +24,8 @@ var decline_post = (post_id, actor_id, gid, cb) => {
             })
         }
     };
-
     let body = Object.keys(post_data).reduce((prev, k) => prev + `${k}=${encodeURIComponent(typeof post_data[k] == 'object' ? JSON.stringify(post_data[k]) : post_data[k])}&`, '');
-
     body += `fb_dtsg=${encodeURIComponent(fb_dtsg)}&__a=1`;
-
     return fetch('https://www.facebook.com/api/graphql/', {
             body,
             method: 'POST',
@@ -40,7 +35,6 @@ var decline_post = (post_id, actor_id, gid, cb) => {
         })
         .then(res => res.json());
 }
-
 var remove_pending_posts = get_pending_posts(gid)
     .then(posts => {
         console.log(`Tìm thấy ${posts.length} bài viết đang chờ duyệt`);
